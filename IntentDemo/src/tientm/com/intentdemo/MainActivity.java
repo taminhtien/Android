51c6574528afae0ef19dc942ad7e095823744a7c
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-04 19:51:00 +0700

    Add Intent demo

---
+++ IntentDemo/src/tientm/com/intentdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package tientm.com.intentdemo;
+
+import com.example.intentdemo.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void btnOpenSecondActivityClick(View v) {
+		// Đối số 1 là intance của Activity hiện tại
+		// Đối số 2 là Activity Mới . Class
+		Intent in = new Intent(MainActivity.this, SecondActivity.class);
+		// Gởi yêu cầu hệ thống mở màn hình Second Activity
+		startActivity(in);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
