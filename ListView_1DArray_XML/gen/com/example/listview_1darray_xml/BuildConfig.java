d70b8159daa7ad26b13da242767dd62885a97dd9
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-07 23:09:34 +0700

    Add List view example. Using string array xml

---
+++ ListView_1DArray_XML/gen/com/example/listview_1darray_xml/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.listview_1darray_xml;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ListView_1DArray_XML/src/com/example/listview_1darray_xml/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.listview_1darray_xml;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class MainActivity extends Activity {
+
+	TextView txtSelected;
+	Button btnShowListView;
+
+	ListView lvData;
+	String arrData[] = null;
+	ArrayAdapter<String> adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		addControls();
+		addEvents();
+	}
+
+	public void addControls() {
+		txtSelected = (TextView) findViewById(R.id.txtSelected);
+		btnShowListView = (Button) findViewById(R.id.btnShowListView);
+		lvData = (ListView) findViewById(R.id.lvData);
+		arrData = getResources().getStringArray(R.array.arr_weeks);
+		adapter = new ArrayAdapter<String>(MainActivity.this,
+				android.R.layout.simple_list_item_1, arrData);
+		lvData.setAdapter(adapter);
+	}
+
+	public void addEvents() {
+		lvData.setOnItemClickListener(new OnItemClickListener() {
+
+			// arg2: position of selected item
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				txtSelected.setText(arrData[arg2]);
+			}
+		});
+	}
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
