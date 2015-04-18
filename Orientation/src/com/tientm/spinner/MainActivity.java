3dd7684e1a5e730b287ef7bbb420063e7df6511a
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-18 18:48:27 +0700

    Add orientation example

---
+++ Orientation/src/com/tientm/spinner/MainActivity.java
@@ -0,0 +1,66 @@
+package com.tientm.spinner;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void processOKClick(View v) {
+		Toast.makeText(MainActivity.this, "Click OK", Toast.LENGTH_LONG).show();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.app.Activity#onConfigurationChanged(android.content.res.Configuration
+	 * )
+	 */
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// TODO Auto-generated method stub
+		super.onConfigurationChanged(newConfig);
+		switch (newConfig.orientation) {
+		case Configuration.ORIENTATION_LANDSCAPE:
+			setContentView(R.layout.activity_landscape);
+			break;
+		case Configuration.ORIENTATION_PORTRAIT:
+			setContentView(R.layout.activity_main);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
